15abd00897080e84484020df5f706393886abd16
RamiYounesTrojette <56765992+RamiYounesTrojette@users.noreply.github.com>
2020-11-22 20:13:32 +0100

    Update master.sh

--- master.sh
+++ master.sh
@@ -22,4 +22,4 @@ sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 sudo kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
 sudo apt-get update -y
-sudo kubeadm init > tokenFile
+sudo kubeadm init
