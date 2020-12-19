0e4e57aeecc601ac84468780bc80e383addf09c9
RamiYounesTrojette <56765992+RamiYounesTrojette@users.noreply.github.com>
2020-12-19 22:08:48 +0100

    Update master.sh

--- master.sh
+++ master.sh
@@ -21,6 +21,6 @@ sudo kubeadm init --ignore-preflight-errors stringSlice
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
-sudo kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+sudo kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
 sudo apt-get update -y
 sudo kubeadm init
