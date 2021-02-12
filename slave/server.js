284dad19c727f2d3d925e733d6ecec486ba6c5d5
RamiYounesTrojette <56765992+RamiYounesTrojette@users.noreply.github.com>
2021-02-12 10:13:59 +0100

    Create server.js

---
+++ slave/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cp = require('child_process');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 8090;
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+        res.send('OK');
+});
+
+app.post('/bind', (req, res) => {
+        console.log('starting kub binding');
+        cp.execFile('../slave.sh', function(err, stdout, stderr){
+               var token = 'sudo ' + req.body.token.replace(/\\n/g, '').replace(/\\\n/g, '');
+                console.log(token);
+                cp.exec(token, function(errr, stdoutt, stderrr){
+                        console.log('bound');
+                        res.send('OK');
+                });
+        });
+});
+app.listen(port, () => {
+        console.log(`Web Server listening at ${port}`);
+});
