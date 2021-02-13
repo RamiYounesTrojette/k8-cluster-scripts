5a7782c6a5c7e4ad109e975eb9c08f0b16215aba
RamiYounesTrojette <56765992+RamiYounesTrojette@users.noreply.github.com>
2021-02-13 13:20:47 +0100

    Update server.js

--- master/server.js
+++ master/server.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const http = require('http');
 const querystring = require('querystring');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 const app = express();
 const port = 8090;
 
@@ -14,6 +15,7 @@ app.get('/', (req, res) => {
         res.send('OK');
 })
 var clusterReady = false;
+var publicKey = "";
 app.get('/checkCluser', (req, res) => {
         if(clusterReady == true)
                 res.send('OK');
@@ -21,35 +23,39 @@ app.get('/checkCluser', (req, res) => {
 ;
 
 app.post('/', (req, res) => {
-        console.log('starting kub binding');
+    console.log('starting kub binding');
+    cp.exec('ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa', function(errr, stdoutt, stderrr){
+        console.log('key generated');  
+        publicKey = fs.readFileSync('~/.ssh/id_rsa.pub', 'utf8');
         cp.execFile('../master.sh', function(err, stdout, stderr){
-                console.log('finished binding');
-                var data = querystring.stringify({
-    token:stdout.substring(stdout.lastIndexOf('kubeadm join'), stdout.lastIndexOf('serviceaccount/weave-net created'))
-  });
-
-  var options = {
-    host: req.body.slave,
-    port: 8090,
-    path: '/bind',
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-      'Content-Length': Buffer.byteLength(data)
-    }
-  };
-console.log('sending token to ' + req.body.slave);
-  var httpreq = http.request(options, function (response) {
-          response.on('end', function() {
-                  console.log('slave bound');
-                  clusterReady = true;
-      res.send('ok');
-    })
-  });
-  httpreq.write(data);
-  httpreq.end();
-                console.log('token sent');
+            console.log('finished binding');
+            var data = querystring.stringify({
+                token:stdout.substring(stdout.lastIndexOf('kubeadm join'), stdout.lastIndexOf('serviceaccount/weave-net created')),
+                key: publicKey
+            });
+            var options = {
+             host: req.body.slave,
+             port: 8090,
+             path: '/bind',
+             method: 'POST',
+             headers: {
+               'Content-Type': 'application/x-www-form-urlencoded',
+               'Content-Length': Buffer.byteLength(data)
+             }
+            };
+            console.log('sending token to ' + req.body.slave);
+            var httpreq = http.request(options, function (response) {
+                response.on('end', function() {
+                    console.log('slave bound');
+                    clusterReady = true;
+                    res.send('ok');
+                });
+            });
+            httpreq.write(data);
+            httpreq.end();
+            console.log('token sent');
         });
+    });
 });
 app.listen(port, () => {
         console.log(`Web Server listening at ${port}`);
