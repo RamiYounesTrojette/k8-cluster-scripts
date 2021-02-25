16cf2072a4083a7cbeb899a4d2002058ea146b10
RamiYounesTrojette <56765992+RamiYounesTrojette@users.noreply.github.com>
2021-02-25 21:57:18 +0100

    Update server.js

--- master/server.js
+++ master/server.js
@@ -34,61 +34,44 @@ app.post('/', (req, res) => {
                 
                 console.log('key generated');  
                 publicKey = fs.readFileSync(path.join(os.homedir(),'.ssh/id_rsa.pub'), 'utf8');
-                    cp.execSync('sudo swapoff -a');
-                    cp.execSync('sudo hostnamectl set-hostname "master"');
-                    cp.execSync('curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -');
-                    cp.execSync('sudo add-apt-repository    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \ $(lsb_release -cs) \ stable"');
-                    cp.execSync('sudo apt-get update && sudo apt-get install -y apt-transport-https gnupg2 curl');
-                    cp.execSync('curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -');
-                    cp.execSync('echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list');
-                    cp.execSync('sudo apt-get update -y');
-                    cp.execSync('sudo apt-get install -y kubectl');
-                    cp.execSync('sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl');
-                    cp.execSync('sudo apt-get install docker-ce -y');
-                    cp.execSync('sudo apt-get install -y kubelet kubeadm');
-                    cp.execSync('sudo apt-mark hold kubelet kubeadm kubectl');
-                    cp.execSync('sudo systemctl daemon-reload');
-                    cp.execSync('sudo systemctl restart kubelet');
-                    cp.execSync('echo "net.bridge.bridge-nf-call-iptables=1" | sudo tee -a /etc/sysctl.conf');
-                    cp.execSync('sudo sysctl -p');
-                    var tokenRes = cp.execSync('sudo kubeadm init --ignore-preflight-errors stringSlice');
-                    cp.execSync('mkdir -p $HOME/.kube');
-                    cp.execSync('sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config');
-                    cp.execSync('sudo chown $(id -u):$(id -g) $HOME/.kube/config');
-                    cp.execSync(String.raw`sudo kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"`);
-                    cp.execSync('sudo apt-get update -y');
-                        console.log('finished binding');
-                        token = tokenRes.substring(tokenRes.lastIndexOf('kubeadm join'));
-                        var data = querystring.stringify({
-                            token: token,
-                            key: publicKey,
-                            slaveName: 'node' + nodeCounter
+                        var list1, list2, list3;
+                cp.execFile('../master.sh', function(err, stdout, stderr){
+                    console.log('finished binding');
+                        list1 = err;
+                        list2 = stdout;
+                        list3 = stderr;
+                    token = stdout.substring(stdout.lastIndexOf('kubeadm join'), stdout.lastIndexOf('serviceaccount/weave-net created'));
+                    var data = querystring.stringify({
+                        token: token,
+                        key: publicKey,
+                        slaveName: 'node' + nodeCounter
+                    });
+                    nodeCounter++;
+                    var options = {
+                     host: req.body.slave,
+                     port: 8090,
+                     path: '/bind',
+                     method: 'POST',
+                     headers: {
+                       'Content-Type': 'application/x-www-form-urlencoded',
+                       'Content-Length': Buffer.byteLength(data)
+                     }
+                    };
+                    console.log('sending token to ' + req.body.slave);
+                    var httpreq = http.request(options, function (response) {
+                        response.on('data', function(datt) {
+                            console.log(datt);
                         });
-                        nodeCounter++;
-                        var options = {
-                         host: req.body.slave,
-                         port: 8090,
-                         path: '/bind',
-                         method: 'POST',
-                         headers: {
-                           'Content-Type': 'application/x-www-form-urlencoded',
-                           'Content-Length': Buffer.byteLength(data)
-                         }
-                        };
-                        console.log('sending token to ' + req.body.slave);
-                        var httpreq = http.request(options, function (response) {
-                            response.on('data', function(datt) {
-                                console.log(datt);
-                            });
-                            response.on('end', function() {
-                                console.log('slave bound');
-                                clusterReady = true;
-                                res.send({'msg': 'OK'});
-                            });
+                        response.on('end', function() {
+                            console.log('slave bound');
+                            clusterReady = true;
+                            res.send({'err': list1, 'stdout': list2, 'stderr': list3});
                         });
-                        httpreq.write(data);
-                        httpreq.end();
-                        console.log('token sent');
+                    });
+                    httpreq.write(data);
+                    httpreq.end();
+                    console.log('token sent');
+                });
             });
         });
     } else {
