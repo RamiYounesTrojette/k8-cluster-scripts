11874b22eaf58f9b04d453f5bc0178080afcb132
RamiYounesTrojette <56765992+RamiYounesTrojette@users.noreply.github.com>
2021-02-28 13:12:10 +0100

    Update master.sh

--- master.sh
+++ master.sh
@@ -9,6 +9,7 @@ sudo add-apt-repository    "deb [arch=amd64] https://download.docker.com/linux/u
 sudo apt-get update && sudo apt-get install -y apt-transport-https gnupg2 curl
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
 sudo apt-get update -y
 sudo apt-get install -y kubectl
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
