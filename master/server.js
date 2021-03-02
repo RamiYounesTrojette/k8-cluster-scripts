a84b9007ae6630e52379b368b91199943bdbd981
RamiYounesTrojette <56765992+RamiYounesTrojette@users.noreply.github.com>
2021-03-02 22:32:03 +0100

    Update server.js

--- master/server.js
+++ master/server.js
@@ -49,7 +49,7 @@ app.post('/', (req, res) => {
                         cp.execSync('kubectl create ns test');
                         cp.execSync('kubectl run mypod --image=chentex/random-logger --namespace=test ');
                     } catch (e) {
-                        except = e;
+                        except = e.stdout;
                     }
                 }
                 console.log('key generated');
