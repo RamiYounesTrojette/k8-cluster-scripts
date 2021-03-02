d5ac483646c40f674d6ab229ee3c1ef70595b9b0
RamiYounesTrojette <56765992+RamiYounesTrojette@users.noreply.github.com>
2021-03-02 18:53:02 +0100

    Update master.sh

--- master.sh
+++ master.sh
@@ -2,17 +2,22 @@
 export DEBIAN_FRONTEND=noninteractive
 sudo swapoff -a
 sudo hostnamectl set-hostname "master"
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-  stable"
+sudo apt remove --yes docker docker-engine docker.io containerd runc
+sudo apt update
+sudo apt --yes --no-install-recommends install apt-transport-https ca-certificates
+wget --quiet --output-document=- https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/ubuntu $(lsb_release --codename --short) stable"
+sudo apt update
+sudo apt --yes --no-install-recommends install docker-ce docker-ce-cli containerd.io
+sudo usermod --append --groups docker "$USER"
+sudo systemctl enable docker
+sudo systemctl start docker
 sudo apt-get update && sudo apt-get install -y apt-transport-https gnupg2 curl
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update -y
 sudo apt-get install -y kubectl
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-sudo apt-get install docker-ce -y
 sudo apt-get install -y kubelet kubeadm
 sudo apt-mark hold kubelet kubeadm kubectl
 sudo systemctl daemon-reload
